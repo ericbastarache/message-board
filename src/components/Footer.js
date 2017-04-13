cfdeaa4c6afd9be07401049896f0ebfa8c662380
Anthony Michelizzi <cynical89@gmail.com>
2017-04-12 20:52:38 -0700

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import React from "react";
+
+const Footer = 	({ footerText }) => {
+	return {
+		<div>
+			<hr>
+			<p>{ footerText }</p>
+		</div>
+	}
+}
+
+Footer.propTypes = {
+	footerText: React.propTypes.string
+}
+
+export default.Footer;
