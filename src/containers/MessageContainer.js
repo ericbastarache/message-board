760943c8e6459ebb8dff5cea15e89dc309d4f08e
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-12 23:45:40 -0400

    Updated message container

---
+++ src/containers/MessageContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MessageContainer extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+
+
+  render () {
+    return (
+      
+    );
+  }
+}
+
+export default MessageContainer;
