3ebaebd4000f7a023d16167d78fc3a2125b72b50
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 00:26:09 -0400

    Added react router to main file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+<<<<<<< Updated upstream
       <div>
         <Header headerText="Message Board Application"/>
 				<br /><p>This is a homepage</p><br />
         <Footer footerText="Copyright 2017"/>
       </div>
+=======
+      <Router>
+        <div>
+          <Header headerText="Message Board Application"/>
+          <Footer footerText="Copyright 2017"/>
+        </div>
+      </Router>
+>>>>>>> Stashed changes
     );
   }
 }
