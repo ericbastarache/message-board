a1778e6443e782e969c2b1827843bf4572ae2b12
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-13 00:27:22 -0400

    Updated router

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-<<<<<<< Updated upstream
+      <Router>
+        <div>
       <div>
         <Header headerText="Message Board Application"/>
 				<br /><p>This is a homepage</p><br />
         <Footer footerText="Copyright 2017"/>
       </div>
-=======
-      <Router>
+
         <div>
           <Header headerText="Message Board Application"/>
           <Footer footerText="Copyright 2017"/>
         </div>
+      </div>
       </Router>
->>>>>>> Stashed changes
     );
   }
 }
